f298a7c53be1a814fce636c7ba02d668eee71c84
Adonan17 <adnaneouassari@gmail.com>
2025-12-11 11:48:45 +0100

    writing get_next_line function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,32 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 17:53:57 by aouassar          #+#    #+#             */
-/*   Updated: 2025/12/03 17:53:58 by aouassar         ###   ########.fr       */
+/*   Updated: 2025/12/08 23:29:50 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static char	*read_and_stash(int fd, char *stash);
+static char	*extract_line(char *stash);
+static char	*clean_stash(char *stash);
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+	
+	if	(fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = read_and_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = extract_line(stash);
+	stash = clean_stash(stash);
+	return (line);
+}
+
+static char	*read_and_stash(int fd, char *stash)
+{
+	
+}
