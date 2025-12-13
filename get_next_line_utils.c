86a92a278d98018597b483b1b2c3583289a48552
Adonan17 <adnaneouassari@gmail.com>
2025-12-13 15:28:24 +0100

    added the needed helpers in the utils file

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,90 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 17:54:34 by aouassar          #+#    #+#             */
-/*   Updated: 2025/12/13 13:53:23 by aouassar         ###   ########.fr       */
+/*   Updated: 2025/12/13 15:27:48 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	slen;
+	size_t	i;
+
+	if (!s)
+		len = 0;
+	slen = ft_strlen(s);
+	if (start >= slen)
+		len = 0;
+	else if (len > slen - start)
+		len = slen - start;
+	sub = (char *)malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	size_t	len1;
+	size_t	len2;
+	size_t	i;
+	size_t	j;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	joined = (char *)malloc(len1 + len2 + 1);
+	if (!joined)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len2)
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+	joined[i + j] = '\0';
+	return (joined);
+}
